427eb95f22cf5c2a59cebd7718f68aaa5bc66572
Tofix26 <73693639+Tofix26@users.noreply.github.com>
2022-06-20 18:36:59 +0200

    started doin shop gui

--- Scripts/Shop.lua
+++ Scripts/Shop.lua
@@ -1,42 +1,37 @@
 dofile "$GAME_DATA/Scripts/game/AnimationUtil.lua"
 
-local renderables =   {"$SURVIVAL_DATA/Character/Char_Tools/Char_logbook/char_logbook.rend" }
-local renderablesTp = {"$SURVIVAL_DATA/Character/Char_Male/Animations/char_male_tp_logbook.rend", "$SURVIVAL_DATA/Character/Char_Tools/Char_logbook/char_logbook_tp_animlist.rend"}
-local renderablesFp = {"$SURVIVAL_DATA/Character/Char_Male/Animations/char_male_fp_logbook.rend", "$SURVIVAL_DATA/Character/Char_Tools/Char_logbook/char_logbook_fp_animlist.rend"}
+local renderables = { "$SURVIVAL_DATA/Character/Char_Tools/Char_logbook/char_logbook.rend" }
+local renderablesTp = { "$SURVIVAL_DATA/Character/Char_Male/Animations/char_male_tp_logbook.rend",
+	"$SURVIVAL_DATA/Character/Char_Tools/Char_logbook/char_logbook_tp_animlist.rend" }
+local renderablesFp = { "$SURVIVAL_DATA/Character/Char_Male/Animations/char_male_fp_logbook.rend",
+	"$SURVIVAL_DATA/Character/Char_Tools/Char_logbook/char_logbook_fp_animlist.rend" }
 
-sm.tool.preloadRenderables( renderables )
-sm.tool.preloadRenderables( renderablesTp )
-sm.tool.preloadRenderables( renderablesFp )
+sm.tool.preloadRenderables(renderables)
+sm.tool.preloadRenderables(renderablesTp)
+sm.tool.preloadRenderables(renderablesFp)
 
 Shop = class()
 
-function Shop.client_onCreate( self )
+function Shop.client_onCreate(self)
 	self.cl = {}
 
 	if self.tool:isLocal() then
-		self.cl.gui = sm.gui.createGuiFromLayout("$CONTENT_DATA/Gui/Shop.layout")
-		
-		local data = sm.json.open("$SURVIVAL_DATA/CraftingRecipes/hideout.json")
-		for k, item in ipairs(data) do
-			self.cl.gui:addGridItem( "TradeGrid", item )
-		end
-		self.cl.gui:setOnCloseCallback( "cl_onGuiClosed" )
-		self.cl.seatedEquiped = false
+		self.cl.gui = sm.gui.createGuiFromLayout("$CONTENT_DATA/Gui/shop.layout")
 	end
 
 	self:client_onRefresh()
 end
 
-function Shop.client_onRefresh( self )
+function Shop.client_onRefresh(self)
 	self:cl_loadAnimations()
 end
 
-function Shop.client_onUpdate( self, dt )
+function Shop.client_onUpdate(self, dt)
 	-- First person animation
 	local isCrouching = self.tool:isCrouching()
 
 	if self.tool:isLocal() then
-		updateFpAnimations( self.fpAnimations, self.cl.equipped, dt )
+		updateFpAnimations(self.fpAnimations, self.cl.equipped, dt)
 	end
 
 	if not self.cl.equipped then
@@ -51,11 +46,11 @@ function Shop.client_onUpdate( self, dt )
 	local normalWeight = 1.0 - crouchWeight
 	local totalWeight = 0.0
 
-	for name, animation in pairs( self.tpAnimations.animations ) do
+	for name, animation in pairs(self.tpAnimations.animations) do
 		animation.time = animation.time + dt
 
 		if name == self.tpAnimations.currentAnimation then
-			animation.weight = math.min( animation.weight + ( self.tpAnimations.blendSpeed * dt ), 1.0 )
+			animation.weight = math.min(animation.weight + (self.tpAnimations.blendSpeed * dt), 1.0)
 
 			if animation.looping == true then
 				if animation.time >= animation.info.duration then
@@ -63,36 +58,36 @@ function Shop.client_onUpdate( self, dt )
 				end
 			end
 			if animation.time >= animation.info.duration - self.cl.blendTime and not animation.looping then
-				if ( name == "putdown" ) then
+				if (name == "putdown") then
 					self.cl.equipped = false
 				elseif animation.nextAnimation ~= "" then
-					setTpAnimation( self.tpAnimations, animation.nextAnimation, 0.001 )
+					setTpAnimation(self.tpAnimations, animation.nextAnimation, 0.001)
 				end
 			end
 		else
-			animation.weight = math.max( animation.weight - ( self.tpAnimations.blendSpeed * dt ), 0.0 )
+			animation.weight = math.max(animation.weight - (self.tpAnimations.blendSpeed * dt), 0.0)
 		end
 
 		totalWeight = totalWeight + animation.weight
 	end
 
 	totalWeight = totalWeight == 0 and 1.0 or totalWeight
-	for name, animation in pairs( self.tpAnimations.animations ) do
+	for name, animation in pairs(self.tpAnimations.animations) do
 
 		local weight = animation.weight / totalWeight
 		if name == "idle" then
-			self.tool:updateMovementAnimation( animation.time, weight )
+			self.tool:updateMovementAnimation(animation.time, weight)
 		elseif animation.crouch then
-			self.tool:updateAnimation( animation.info.name, animation.time, weight * normalWeight )
-			self.tool:updateAnimation( animation.crouch.name, animation.time, weight * crouchWeight )
+			self.tool:updateAnimation(animation.info.name, animation.time, weight * normalWeight)
+			self.tool:updateAnimation(animation.crouch.name, animation.time, weight * crouchWeight)
 		else
-			self.tool:updateAnimation( animation.info.name, animation.time, weight )
+			self.tool:updateAnimation(animation.info.name, animation.time, weight)
 		end
 	end
 
 end
 
-function Shop.client_onEquip( self )
+function Shop.client_onEquip(self)
 	self.cl.wantsEquip = true
 	self.cl.seatedEquiped = false
 
@@ -104,40 +99,41 @@ function Shop.client_onEquip( self )
 	concat(currentRenderablesFp, renderablesFp)
 	concat(currentRenderablesFp, renderables)
 
-	self.tool:setTpRenderables( currentRenderablesTp )
+	self.tool:setTpRenderables(currentRenderablesTp)
 
 	if self.tool:isLocal() then
-		self.tool:setFpRenderables( currentRenderablesFp )
+		self.tool:setFpRenderables(currentRenderablesFp)
 		self.cl.gui:open()
 	end
 
 	self:cl_loadAnimations()
-	setTpAnimation( self.tpAnimations, "pickup", 0.0001 )
+	setTpAnimation(self.tpAnimations, "pickup", 0.0001)
 
 	if self.tool:isLocal() then
-		swapFpAnimation( self.fpAnimations, "unequip", "equip", 0.2 )
+		swapFpAnimation(self.fpAnimations, "unequip", "equip", 0.2)
 	end
 end
 
-function Shop.client_equipWhileSeated( self )
+function Shop.client_equipWhileSeated(self)
 	if not self.cl.seatedEquiped then
 		self.cl.gui:open()
 		self.cl.seatedEquiped = true
 	end
 end
 
-function Shop.client_onUnequip( self )
+function Shop.client_onUnequip(self)
 	self.cl.wantsEquip = false
 	self.cl.seatedEquiped = false
-	if sm.exists( self.tool ) then
-		setTpAnimation( self.tpAnimations, "useExit" )
-		if self.tool:isLocal() and self.fpAnimations.currentAnimation ~= "unequip" and self.fpAnimations.currentAnimation ~= "useExit" then
-			swapFpAnimation( self.fpAnimations, "equip", "useExit", 0.2 )
+	if sm.exists(self.tool) then
+		setTpAnimation(self.tpAnimations, "useExit")
+		if self.tool:isLocal() and self.fpAnimations.currentAnimation ~= "unequip" and
+			self.fpAnimations.currentAnimation ~= "useExit" then
+			swapFpAnimation(self.fpAnimations, "equip", "useExit", 0.2)
 		end
 	end
 end
 
-function Shop.cl_loadAnimations( self )
+function Shop.cl_loadAnimations(self)
 	-- TP
 	self.tpAnimations = createTpAnimations(
 		self.tool,
@@ -172,8 +168,8 @@ function Shop.cl_loadAnimations( self )
 		crouchBwd = "logbook_crouch_bwd"
 	}
 
-	for name, animation in pairs( movementAnimations ) do
-		self.tool:setMovementAnimation( name, animation )
+	for name, animation in pairs(movementAnimations) do
+		self.tool:setMovementAnimation(name, animation)
 	end
 
 	if self.tool:isLocal() then
@@ -190,12 +186,12 @@ function Shop.cl_loadAnimations( self )
 		)
 	end
 
-	setTpAnimation( self.tpAnimations, "idle", 5.0 )
+	setTpAnimation(self.tpAnimations, "idle", 5.0)
 	self.cl.blendTime = 0.2
 
 end
 
-function Shop.cl_onGuiClosed( self )
-	sm.tool.forceTool( nil )
+function Shop.cl_onGuiClosed(self)
+	sm.tool.forceTool(nil)
 	self.cl.seatedEquiped = false
 end
