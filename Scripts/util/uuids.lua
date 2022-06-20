8e5fb1347270b6150ea98e02d03b5dee538450a5
Tofix26 <73693639+Tofix26@users.noreply.github.com>
2022-06-20 17:45:04 +0200

    auto generated uuids

---
+++ Scripts/util/uuids.lua
@@ -0,0 +1,117 @@
+---@diagnostic disable: lowercase-global
+-- this file is generated
+
+----------------------------------------
+-- shapesets.shapedb
+----------------------------------------
+
+----------------------------------------
+-- furnaces.shapeset
+----------------------------------------
+
+-- partList
+
+obj_furnace_basic = sm.uuid.new("7e3df19b-2450-44ae-ad46-d2f6b5148cbf")
+
+----------------------------------------
+-- droppers.shapeset
+----------------------------------------
+
+-- partList
+
+obj_dropper_scrap_wood = sm.uuid.new("692a5ebd-0793-49ba-b1ef-681a8fdceba7")
+
+----------------------------------------
+-- drops.shapeset
+----------------------------------------
+
+-- partList
+
+obj_drop_scrap_wood = sm.uuid.new("87873e83-8270-4796-9401-6baba45ccef2")
+
+----------------------------------------
+-- upgraders.shapeset
+----------------------------------------
+
+-- partList
+
+obj_upgrader_basic = sm.uuid.new("69b869f1-03dc-4ea2-9391-fd24903945d1")
+obj_upgrader_toilet = sm.uuid.new("e2a6970b-2bb9-4b3b-b9f2-994b534b79f3")
+
+----------------------------------------
+-- belts.shapeset
+----------------------------------------
+
+-- partList
+
+obj_belt1_straight = sm.uuid.new("69b857f1-07dc-4ea2-9391-fd24903945d1")
+
+----------------------------------------
+-- generators.shapeset
+----------------------------------------
+
+-- partList
+
+obj_generator_windmill = sm.uuid.new("daf6f4c4-5402-4fa3-93f4-3180243c8a3c")
+
+----------------------------------------
+-- crates.shapeset
+----------------------------------------
+
+-- partList
+
+obj_lootcrate = sm.uuid.new("69b869f1-03dc-4ea3-9291-fd6490f945dd")
+
+----------------------------------------
+-- scriptableObjectSets.sobdb
+----------------------------------------
+
+----------------------------------------
+-- example.sobset
+----------------------------------------
+
+-- scriptableObjectList
+
+
+----------------------------------------
+-- harvestablesets.harvestabledb
+----------------------------------------
+
+----------------------------------------
+-- example.harvestableset
+----------------------------------------
+
+-- harvestableList
+
+
+----------------------------------------
+-- toolsets.tooldb
+----------------------------------------
+
+----------------------------------------
+-- example.toolset
+----------------------------------------
+
+-- toolList
+
+tool_shop = sm.uuid.new("a6113860-f4d4-42df-b8f9-129efdbaf777")
+tool_lift = sm.uuid.new("5cc12f03-275e-4c8e-b013-79fc0f913e1b")
+tool_hammer = sm.uuid.new("ed185725-ea12-43fc-9cd7-4295d0dbf88b")
+
+----------------------------------------
+-- charactersets.characterdb
+----------------------------------------
+
+----------------------------------------
+-- example.characterset
+----------------------------------------
+
+-- characters
+
+
+----------------------------------------
+-- projectiles.projectileset
+----------------------------------------
+
+-- projectiles
+
