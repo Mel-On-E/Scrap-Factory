0fd195cd61c0e5b950f922491c1a039d26d0be53
Dr-Pixel-Plays <86497424+Dr-Pixel-Plays@users.noreply.github.com>
2022-06-23 22:50:08 +0200

    Windmills give more power when higher

--- Scripts/Generators/Generator.lua
+++ Scripts/Generators/Generator.lua
@@ -13,3 +13,14 @@ function Generator:server_onFixedUpdate()
         sm.event.sendToGame("sv_e_addPower", self.data.power)
     end
 end
+
+Windmill = class(Generator)
+
+function Windmill:server_onFixedUpdate()
+    if self.data.power > 0 and sm.game.getCurrentTick() % 40 == 0 then
+        local heightMultiplier = math.max(self.shape.worldPosition.z/100 + 1, 1)
+        local power = math.min(math.floor(heightMultiplier*self.data.power), self.data.power*2)
+        print(power)
+        sm.event.sendToGame("sv_e_addPower", power)
+    end
+end
