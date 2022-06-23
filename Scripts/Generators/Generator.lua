074280eb1dd1c49c7e566e88dae851c8809604d1
Dr-Pixel-Plays <86497424+Dr-Pixel-Plays@users.noreply.github.com>
2022-06-23 22:58:36 +0200

    windmills also scale power limit

--- Scripts/Generators/Generator.lua
+++ Scripts/Generators/Generator.lua
@@ -16,11 +16,22 @@ end
 
 Windmill = class(Generator)
 
+function Windmill:server_onCreate()
+    self.powerLimit = self:getPower(self.data.powerLimit)
+    sm.event.sendToGame("sv_e_addPowerLimit", self.powerLimit)
+end
+
+function Windmill:server_onDestroy()
+    sm.event.sendToGame("sv_e_addPowerLimit", -self.powerLimit)
+end
+
 function Windmill:server_onFixedUpdate()
-    if self.data.power > 0 and sm.game.getCurrentTick() % 40 == 0 then
-        local heightMultiplier = math.max(self.shape.worldPosition.z/100 + 1, 1)
-        local power = math.min(math.floor(heightMultiplier*self.data.power), self.data.power*2)
-        print(power)
-        sm.event.sendToGame("sv_e_addPower", power)
+    if sm.game.getCurrentTick() % 40 == 0 then
+        sm.event.sendToGame("sv_e_addPower", self:getPower(self.data.power))
     end
+end
+
+function Windmill:getPower(power)
+    local heightMultiplier = math.max(self.shape.worldPosition.z/100 + 1, 1)
+    return math.min(math.floor(heightMultiplier*power), power*2)
 end
