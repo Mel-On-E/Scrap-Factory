30467fb5092265cdad8151cc3c87d8e85dc13a04
Dr-Pixel-Plays <86497424+Dr-Pixel-Dev@users.noreply.github.com>
2022-08-10 18:16:18 +0200

    Revert "more shid (borken)"
    This reverts commit 496150519693b707f7a9a1eb534ecf555fc60984.

--- Scripts/util.lua
+++ Scripts/util.lua
@@ -1,8 +1,10 @@
 ---@diagnostic disable: lowercase-global
----@param money number
-function format_money(money, color)
-    if not color then
-        color = "#00dd00"
+
+--TODO fix duplication in format methods
+
+function format_money(params)
+    if not params.color then
+        params.color = "#00dd00"
     end
 
     local suffixes = {}
@@ -11,12 +13,12 @@ function format_money(money, color)
         suffixes[k*3] = letter
     end
 
-    moneyStr = tostring(math.floor(money))
+    moneyStr = tostring(math.floor(params.money))
     local length = #moneyStr
     local suffix = ""
 
     if length < 3 then
-        return string.format(color .. "$%.2f", money)
+        return string.format(params.color .. "$%.2f", params.money)
     end
 
     for len, suf in pairs(suffixes) do
@@ -34,12 +36,15 @@ function format_money(money, color)
     if #leadingDigits == 0 then
         separator = ""
     end
-    return color .. "$" .. leadingDigits .. separator .. followingDigits .. suffix
+    return params.color .. "$" .. leadingDigits .. separator .. followingDigits .. suffix
 end
 
-function format_energy( power, color)
-    if not color then
-        color = "#dddd00"
+function format_energy(params)
+    if not params.color then
+        params.color = "#dddd00"
+    end
+    if not params.unit then
+        params.unit = "W"
     end
 
     local suffixes = {}
@@ -48,12 +53,12 @@ function format_energy( power, color)
         suffixes[k*3] = letter
     end
 
-    powerStr = tostring(math.floor(power))
+    powerStr = tostring(math.floor(params.power))
     local length = #powerStr
     local suffix = ""
 
     if length < 3 then
-        return string.format(color .. power .. "Wh")
+        return string.format(params.color .. params.power .. params.unit)
     end
 
     for len, suf in pairs(suffixes) do
@@ -71,15 +76,17 @@ function format_energy( power, color)
     if #leadingDigits == 0 then
         separator = ""
     end
-    return color .. leadingDigits .. separator .. followingDigits .. suffix .. "W"
+    return params.color .. leadingDigits .. separator .. followingDigits .. suffix .. params.unit
 end
 
-function consume_power(power)
-    if g_power >= power then
-        g_power = g_power - power
-        return true
-    else
-        g_power = 0
-        return false
+function change_power(power)
+    g_power = g_power + power
+    return g_powerStored + g_power > 0
+end
+
+function change_power_storage(capactiy)
+    g_powerLimit = g_powerLimit + capactiy
+    if g_powerLimit < 0 then
+        sm.gui.chatMessage("#ff0000IF YOU ARE SEEING THIS PLS REPORT TO THE DEVS: POWERLIMIT < 0")
     end
 end
