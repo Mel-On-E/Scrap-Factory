7421941ee7bf14393ef546f1caaf6a635f675c47
Dr-Pixel-Dev <86497424+Dr-Pixel-Dev@users.noreply.github.com>
2022-08-13 19:23:54 +0200

    Finished Daily Reward System

--- Scripts/Managers/DailyRewardManager.lua
+++ Scripts/Managers/DailyRewardManager.lua
@@ -5,34 +5,51 @@ dofile("$CONTENT_DATA/Scripts/util/util.lua")
 DailyRewardManager = class()
 DailyRewardManager.isSaveObject = true
 
-local DAY = 1--1000*60*60*20 --20 Hours
+local DAY = 1000*60*60*20 --20 Hours
 
 function DailyRewardManager:server_onCreate()
     self.saved = self.storage:load()
 
     if self.saved == nil then
         self.saved = {}
-        self.saved.time = os.time()
+        self.saved.time = os.time() - DAY
+        self.storage:save(self.saved)
+    end
+
+    if os.time() - self.saved.time >= DAY*2 then
         self.saved.streak = 0
         self.storage:save(self.saved)
     end
 
-    if os.time() - self.saved.time > DAY*2 then
-        self.saved.streak = 0
-        self.storage:save(self.saved)
-    end
-
-    if os.time() - self.saved.time > DAY then
+    if os.time() - self.saved.time >= DAY then
         self.network:sendToClients("cl_openGui")
     end
+
+    self.rewards = sm.json.open("$CONTENT_DATA/Scripts/daily rewards.json")
+
+    self.network:setClientData({day = self.saved.streak + 1})
 end
 
 function DailyRewardManager:sv_spawnRewards(params, player)
-    local pos = player:getCharacter():getWorldPosition()
-    pos.x = pos.x + (math.random()-0.5)*10
-    pos.y = pos.y + (math.random()-0.5)*10
-    pos.z = pos.z + 5
-    LootCrateManager.sv_spawnCrate(pos)
+    for i = 1, self.rewards[self.saved.streak + 1].quantity, 1 do
+        local pos = player:getCharacter():getWorldPosition()
+        pos.x = pos.x + (math.random()-0.5)*10
+        pos.y = pos.y + (math.random()-0.5)*10
+        pos.z = pos.z + 5
+        LootCrateManager.sv_spawnCrate(pos, self.rewards[self.saved.streak + 1].crate)
+    end
+    self.saved.streak = math.min(self.saved.streak + 1, #self.rewards - 1)
+    self.storage:save(self.saved)
+end
+
+function DailyRewardManager:client_onCreate()
+    self.cl = {}
+    self.cl.day = 1
+end
+
+function DailyRewardManager:client_onClientDataUpdate(data)
+    self.cl.day = data.day
+    self.cl.currentDay = data.day
 end
 
 function DailyRewardManager:cl_openGui()
@@ -44,16 +61,49 @@ function DailyRewardManager:cl_openGui()
         self.gui = sm.gui.createGuiFromLayout("$CONTENT_DATA/Gui/Layouts/DailyReward.layout")
         self.gui:setOnCloseCallback("cl_openGui")
         self.gui:setButtonCallback("Claim Reward", "cl_claimReward")
+        self.gui:setButtonCallback("DayUp", "cl_dayUp")
+        self.gui:setButtonCallback("DayDown", "cl_dayDown")
 
         self.gui:setText("title", language_tag("DailyRewardTitle"))
         self.gui:setText("ClaimRewardText", language_tag("ClaimDailyReward"))
+
+        generateDays(self)
+        
 
         self.gui:open()
     end
 end
 
+function generateDays(self)
+    local offset = math.min(self.cl.day - 1, #self.rewards - 5)
+    for i = 1, 5, 1 do
+        local rewardDay = self.cl.currentDay == i + offset
+        local color = rewardDay and "#dddd00" or "#aaaaaa"
+
+        self.gui:setButtonState("Reward_" .. tostring(i), rewardDay)
+
+        self.gui:setText("RewardText_" .. tostring(i), color .. language_tag("DailyRewardDay"):format(tostring(i + offset)))
+        local uuid = sm.uuid.new(self.rewards[i + offset].crate)
+        self.gui:setIconImage("RewardPic_" .. tostring(i), uuid)
+        self.gui:setText("RewardCount_" .. tostring(i), tostring(self.rewards[i + offset].quantity))
+    end
+end
+
+function DailyRewardManager:cl_dayUp()
+    self.cl.day = math.min(self.cl.day + 1, #self.rewards)
+    generateDays(self)
+end
+
+function DailyRewardManager:cl_dayDown()
+    self.cl.day = math.max(self.cl.day - 1, 1)
+    generateDays(self)
+end
+
 function DailyRewardManager:cl_claimReward()
     self.gui:close()
+    self.gui:destroy()
+    self.gui = nil
     self.claimed = true
     self.network:sendToServer("sv_spawnRewards")
 end
+
--- Scripts/Managers/LootcrateManager.lua
+++ Scripts/Managers/LootcrateManager.lua
@@ -4,10 +4,13 @@ dofile("$CONTENT_DATA/Scripts/util/util.lua")
 
 LootCrateManager = class()
 
+local crates = {}
+crates["69b869f1-03dc-4ea3-9291-fd6490f945dd"] = "normal_crate.blueprint"
+
 function LootCrateManager:server_onFixedUpdate()
     if sm.game.getCurrentTick() % (40 * 30) == 0 and g_world then
 		local pos = sm.vec3.new(math.random(-(15 * 64), 15 * 64), math.random(-(15 * 64), 15 * 64), 100)
-		self.sv_spawnCrate(pos)
+		self.sv_spawnCrate(pos, "69b869f1-03dc-4ea3-9291-fd6490f945dd")
 	end
 end
 
@@ -15,6 +18,6 @@ function LootCrateManager.sv_setWorld(world)
     g_world = world
 end
 
-function LootCrateManager.sv_spawnCrate(pos)
-    sm.creation.importFromFile(g_world, "$CONTENT_DATA/LocalBlueprints/crate.blueprint", pos)
+function LootCrateManager.sv_spawnCrate(pos, uuid)
+    sm.creation.importFromFile(g_world, "$CONTENT_DATA/LocalBlueprints/" .. crates[uuid], pos)
 end
