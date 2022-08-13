fbf2ef78bf3a30a419ee16c206a524d3615e8c90
Dr-Pixel-Dev <86497424+Dr-Pixel-Dev@users.noreply.github.com>
2022-08-14 00:42:02 +0200

    Generators show current production

--- Scripts/Generators/Generator.lua
+++ Scripts/Generators/Generator.lua
@@ -21,11 +21,29 @@ end
 
 function Generator:server_onFixedUpdate()
     if self.data.power and sm.game.getCurrentTick() % 40 == 0 then
-        PowerManager.sv_changePower(self:getPower())
+        local power = self:getPower()
+        PowerManager.sv_changePower(power)
+        self.network:setClientData({power = power})
     end
 end
 
 function Generator:getPower()
     print(self.data.power)
     return self.data.power
+end
+
+function Generator:client_onCreate()
+    self.cl = {}
+    self.cl.power = 0
+end
+
+function Generator:client_onClientDataUpdate(data)
+    self.cl.power = data.power
+end
+
+function Generator:client_canInteract()
+    local o1 = "<p textShadow='false' bg='gui_keybinds_bg_orange' color='#4f4f4f' spacing='9'>"
+    local o2 = "</p>"
+    sm.gui.setInteractionText(language_tag("PowerOutput"), o1 .. format_energy({power = self.cl.power, color = "#4f4f4f"}) .. o2)
+    return true
 end
--- Scripts/Generators/Storage.lua
+++ Scripts/Generators/Storage.lua
@@ -6,6 +6,6 @@ Storage = class( Generator )
 function Storage:client_canInteract()
     local o1 = "<p textShadow='false' bg='gui_keybinds_bg_orange' color='#4f4f4f' spacing='9'>"
     local o2 = "</p>"
-    sm.gui.setInteractionText(o1 .. language_tag("PowerCapacity") .. format_energy({power = PowerManager.cl_getPowerStored(), color = "#4f4f4f", unit = "J"}) .. " / " .. format_energy({power = PowerManager.cl_getPowerLimit(), color = "#4f4f4f", unit = "J"}) .. o2)
+    sm.gui.setInteractionText(language_tag("PowerCapacity"), o1 .. format_energy({power = PowerManager.cl_getPowerStored(), color = "#4f4f4f", unit = "J"}) .. " / " .. format_energy({power = PowerManager.cl_getPowerLimit(), color = "#4f4f4f", unit = "J"}) .. o2)
     return true
 end
