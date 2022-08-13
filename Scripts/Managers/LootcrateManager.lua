530d4de071b6370d27c677da4999ceb0e373222a
Dr-Pixel-Dev <86497424+Dr-Pixel-Dev@users.noreply.github.com>
2022-08-14 00:18:39 +0200

    New Crate

--- Scripts/Managers/LootcrateManager.lua
+++ Scripts/Managers/LootcrateManager.lua
@@ -6,6 +6,7 @@ LootCrateManager = class()
 
 local crates = {}
 crates["69b869f1-03dc-4ea3-9291-fd6490f945dd"] = "normal_crate.blueprint"
+crates["cf48ca1b-3d7a-4b56-83e9-092e681525be"] = "rare_crate.blueprint"
 
 function LootCrateManager:server_onFixedUpdate()
     if sm.game.getCurrentTick() % (40 * 30) == 0 and g_world then
---
+++ Scripts/Other/RareLootCrate.lua
@@ -0,0 +1,34 @@
+---@class RareLootCrate:ShapeClass
+
+dofile("$CONTENT_DATA/Scripts/Other/LootCrate.lua")
+
+RareLootCrate = class( LootCrate )
+
+function RareLootCrate:get_loot_table()
+    local itemPool = {}
+    for uuid, item in pairs(g_shop) do
+        if item.price <= MoneyManager.cl_moneyEarned()*2 + 5000 then
+            itemPool[#itemPool+1] = {price = item.price, uuid = uuid}
+        end
+    end
+
+    local sortedPool = {}
+    while #itemPool > 1 and #sortedPool < 5 do
+        local mostExpensiveItem 
+        local highestPrice = 0
+
+        for k, item in ipairs(itemPool) do
+            if item.price > highestPrice then
+                highestPrice = item.price
+                mostExpensiveItem = k
+            end
+        end
+
+        sortedPool[#sortedPool+1] = sm.uuid.new(itemPool[mostExpensiveItem].uuid)
+        table.remove(itemPool, mostExpensiveItem)
+    end
+
+    sortedPool[#sortedPool+1] = sm.uuid.new( "f08d772f-9851-400f-a014-d847900458a7" )
+
+    return sortedPool
+end
