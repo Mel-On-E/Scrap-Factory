e9a91dd0bbf7c56b7373105d3d4984d3db784879
Tofix26 <73693639+Tofix26@users.noreply.github.com>
2022-09-17 19:37:22 +0200

    Updated uuid generator and regenerated the file

--- Scripts/util/uuids.lua
+++ Scripts/util/uuids.lua
@@ -1,130 +1,75 @@
 ---@diagnostic disable: lowercase-global
 -- this file is generated
 
-----------------------------------------
--- shapesets.shapedb
-----------------------------------------
-
-----------------------------------------
 -- furnaces.shapeset
-----------------------------------------
 
--- partList
+obj_furnace_basic = sm.uuid.new("obj_furnace_basic")
+obj_furnace_2x = sm.uuid.new("obj_furnace_2x")
+obj_furnace_lunar = sm.uuid.new("obj_furnace_lunar")
 
-obj_furnace_basic = sm.uuid.new("7e3df19b-2450-44ae-ad46-d2f6b5148cbf")
-
-----------------------------------------
 -- droppers.shapeset
-----------------------------------------
 
--- partList
+obj_dropper_scrap_wood = sm.uuid.new("obj_dropper_scrap_wood")
+obj_dropper_scrap_wood_auto = sm.uuid.new("obj_dropper_scrap_wood_auto")
 
-obj_dropper_scrap_wood = sm.uuid.new("692a5ebd-0793-49ba-b1ef-681a8fdceba7")
-obj_dropper_scrap_wood_auto = sm.uuid.new("632a52bd-0791-49ba-b1ef-c81adfdceba7")
-
-----------------------------------------
 -- drops.shapeset
-----------------------------------------
 
--- partList
+obj_drop_scrap_wood = sm.uuid.new("obj_drop_scrap_wood")
 
-obj_drop_scrap_wood = sm.uuid.new("87873e83-8270-4796-9401-6baba45ccef2")
-
-----------------------------------------
 -- upgraders.shapeset
-----------------------------------------
 
--- partList
+obj_upgrader_basic = sm.uuid.new("obj_upgrader_basic")
+obj_upgrader_random = sm.uuid.new("obj_upgrader_random")
+obj_upgrader_toilet = sm.uuid.new("obj_upgrader_toilet")
+obj_upgrader_tp_random = sm.uuid.new("obj_upgrader_tp_random")
 
-obj_upgrader_basic = sm.uuid.new("69b869f1-03dc-4ea2-9391-fd24903945d1")
-obj_upgrader_toilet = sm.uuid.new("e2a6970b-2bb9-4b3b-b9f2-994b534b79f3")
-
-----------------------------------------
 -- belts.shapeset
-----------------------------------------
 
--- partList
+obj_belt1_straight = sm.uuid.new("obj_belt1_straight")
 
-obj_belt1_straight = sm.uuid.new("69b857f1-07dc-4ea2-9391-fd24903945d1")
-
-----------------------------------------
 -- generators.shapeset
-----------------------------------------
 
--- partList
+obj_generator_windmill = sm.uuid.new("obj_generator_windmill")
+obj_generator_solar = sm.uuid.new("obj_generator_solar")
+obj_generator_campfire = sm.uuid.new("obj_generator_campfire")
 
-obj_generator_windmill = sm.uuid.new("daf6f4c4-5402-4fa3-93f4-3180243c8a3c")
-obj_generator_solar = sm.uuid.new("efc10452-8461-44c5-8c21-a06d5aaea16c")
-obj_generator_campfire = sm.uuid.new("efc1f452-8431-44c5-8c21-a06d53aed16c")
-
-----------------------------------------
 -- storage.shapeset
-----------------------------------------
 
--- partList
+obj_storage_battery = sm.uuid.new("obj_storage_battery")
 
-obj_storage_battery = sm.uuid.new("91037f2c-5230-464b-8873-ad13355539af")
-
-----------------------------------------
 -- crates.shapeset
-----------------------------------------
 
--- partList
+obj_lootcrate = sm.uuid.new("obj_lootcrate")
+obj_lootcrate_rare = sm.uuid.new("obj_lootcrate_rare")
 
-obj_lootcrate = sm.uuid.new("69b869f1-03dc-4ea3-9291-fd6490f945dd")
+-- decor.shapeset
 
-----------------------------------------
--- scriptableObjectSets.sobdb
-----------------------------------------
+obj_decor_babyduck = sm.uuid.new("obj_decor_babyduck")
 
-----------------------------------------
--- example.sobset
-----------------------------------------
+-- managers.sobset
 
--- scriptableObjectList
+mgr_language = sm.uuid.new("mgr_language")
+mgr_money = sm.uuid.new("mgr_money")
+mgr_power = sm.uuid.new("mgr_power")
+mgr_research = sm.uuid.new("mgr_research")
+mgr_pollution = sm.uuid.new("mgr_pollution")
+mgr_daily = sm.uuid.new("mgr_daily")
+mgr_lootcrate = sm.uuid.new("mgr_lootcrate")
 
+-- interfaces.sobset
 
-----------------------------------------
--- harvestablesets.harvestabledb
-----------------------------------------
+mgr_shop = sm.uuid.new("mgr_shop")
+mgr_research = sm.uuid.new("mgr_research")
 
-----------------------------------------
 -- example.harvestableset
-----------------------------------------
-
--- harvestableList
 
 
-----------------------------------------
--- toolsets.tooldb
-----------------------------------------
-
-----------------------------------------
 -- example.toolset
-----------------------------------------
 
--- toolList
+tool_hub = sm.uuid.new("tool_hub")
+tool_sell = sm.uuid.new("tool_sell")
+tool_lift = sm.uuid.new("tool_lift")
+tool_hammer = sm.uuid.new("tool_hammer")
 
-tool_shop = sm.uuid.new("a6113860-f4d4-42df-b8f9-129efdbaf777")
-tool_research = sm.uuid.new("83cf75e8-9805-4ddf-96f0-6a5e7810d09d")
-tool_sell = sm.uuid.new("8f190ce2-3a59-423e-8483-a7aa67bd5bc0")
-tool_lift = sm.uuid.new("5cc12f03-275e-4c8e-b013-79fc0f913e1b")
-tool_hammer = sm.uuid.new("ed185725-ea12-43fc-9cd7-4295d0dbf88b")
-
-----------------------------------------
--- charactersets.characterdb
-----------------------------------------
-
-----------------------------------------
 -- example.characterset
-----------------------------------------
-
--- characters
-
-
-----------------------------------------
--- projectiles.projectileset
-----------------------------------------
-
--- projectiles
 
