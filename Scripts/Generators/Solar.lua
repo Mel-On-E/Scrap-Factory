c65db35e9049f08105f7c4f4a7609d574ae26f98
Dr-Pixel-Plays <86497424+Dr-Pixel-Dev@users.noreply.github.com>
2022-10-26 23:06:41 +0200

    Solar panel uses day utility now

--- Scripts/Generators/Solar.lua
+++ Scripts/Generators/Solar.lua
@@ -1,24 +1,20 @@
 dofile("$CONTENT_DATA/Scripts/Generators/Generator.lua")
+dofile("$CONTENT_DATA/Scripts/uitl/day.lua")
 
 
 ---@class Solar : Generator
 Solar = class(Generator)
 
-local sunRiseStart = 0.16
-local sunRiseEnd = 0.24
-local sunSetStart = 0.76
-local sunSetEnd = 0.84
-
 function Solar:getPower()
     local time = sm.storage.load(STORAGE_CHANNEL_TIME).timeOfDay
     local timeMultiplier = 0
 
-    if time > sunRiseStart and time < sunRiseEnd then
-        timeMultiplier = (time - sunRiseStart) / (sunRiseEnd - sunRiseStart)
-    elseif time > sunRiseEnd and time < sunSetStart then
+    if time > SunRiseStart and time < SunRiseEnd then
+        timeMultiplier = (time - SunRiseStart) / (SunRiseEnd - SunRiseStart)
+    elseif time > SunRiseEnd and time < SunSetStart then
         timeMultiplier = 1
-    elseif time > sunSetStart and time < sunSetEnd then
-        timeMultiplier = (time - sunSetStart) / (sunSetEnd - sunSetStart)
+    elseif time > SunSetStart and time < SunSetEnd then
+        timeMultiplier = (time - SunSetStart) / (SunSetEnd - SunSetStart)
     end
 
     return math.floor(timeMultiplier * self.data.power)
--- Scripts/util/day.lua
+++ Scripts/util/day.lua
@@ -1,2 +1,4 @@
+SunRiseStart = 0.16
 SunRiseEnd = 0.24
 SunSetStart = 0.76
+SunSetEnd = 0.84
