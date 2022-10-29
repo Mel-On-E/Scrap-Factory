54e363792f2530e3d55282c4fa1940f33eaf8db7
Tofix.js <73693639+Tofix26@users.noreply.github.com>
2022-10-29 08:56:44 +0200

    Day sesnor now detects day and not night

--- Scripts/Other/DaySensor.lua
+++ Scripts/Other/DaySensor.lua
@@ -1,31 +1,32 @@
 dofile("$CONTENT_DATA/Scripts/util/day.lua")
 
 ---@class DayDetector : ShapeClass
----@field night boolean
+---@field day boolean
 ---@diagnostic disable-next-line: assign-type-mismatch
 DaySensor = class()
 DaySensor.connectionOutput = 1
-DaySensor.maxChildCount = 10
+DaySensor.maxChildCount = 255
 DaySensor.poseWeightCount = 1
 
-
+local enabledPose = 10
+local disabledPose = 0
 
 function DaySensor:client_onCreate()
-    self.night = false
+    self.day = false
 end
 
 function DaySensor:server_onFixedUpdate()
     local time = sm.game.getTimeOfDay()
-    local night = time < SunRiseEnd or time > SunSetStart
+    local day = not (time < SunRiseEnd or time > SunSetStart)
 
-    if night == self.night then return end
+    if day == self.day then return end
 
-    self.interactable:setActive(night)
-    self.network:sendToClients("cl_changeModel", night)
-    self.night = night
+    self.interactable:setActive(day)
+    self.network:sendToClients("cl_changeModel", day)
+    self.day = day
 end
 
 ---@param enable boolean If true makes the model "enabled" if false "disabled"
 function DaySensor:cl_changeModel(enable)
-    self.interactable:setUvFrameIndex(enable and 10 or 0)
+    self.interactable:setUvFrameIndex(enable and enabledPose or disabledPose)
 end
