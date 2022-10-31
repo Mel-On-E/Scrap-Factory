dbdd045621db9844c57aaa2559ba3da7886efca4
Tofix26 <73693639+Tofix26@users.noreply.github.com>
2022-10-31 11:12:39 +0100

    Update global.lua

--- Scripts/util/global.lua
+++ Scripts/util/global.lua
@@ -6,3 +6,55 @@ g_factoryHud = {}
 
 ---@type ShopDb
 g_shop = {}
+
+---@type Interactable
+g_research_furnace = {}
+
+---@type boolean
+g_godMode = false
+
+---@type boolean
+g_survivalDev = false
+
+---@type boolean
+g_enableCollisionTumble = false
+
+---@type table
+g_eventManager = {}
+
+---@type table
+g_unitManager = {}
+
+---@type table
+g_beaconManager = {}
+
+---@type table
+g_respawnManager = {}
+
+---@type World
+g_world = {}
+
+---@type table
+g_effectManager = {}
+
+---@type Effect
+g_survivalMusic = {}
+
+---@type GuiInterface
+g_survivalHud = {}
+
+---@class languageManager
+---@field language string Current Language
+g_languageManager = {}
+
+---@type MoneyManager
+g_moneyManager = {}
+
+---@type PerkManager
+g_perkManager = {}
+
+---@type PollutionManager
+g_pollutionManager = {}
+
+---@type PowerManager
+g_powerManager = {}
