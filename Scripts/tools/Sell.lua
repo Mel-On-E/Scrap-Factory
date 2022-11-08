3bbf32c5c4b1879a4fde260efbac6a54f093c9be
Dr-Pixel-Plays <86497424+Dr-Pixel-Dev@users.noreply.github.com>
2022-11-08 15:56:37 +0100

    Sell tool doesn't spam SoftPop when not unlocked

--- Scripts/tools/Sell.lua
+++ Scripts/tools/Sell.lua
@@ -113,7 +113,7 @@ function Sell.client_onUnequip(self)
 end
 
 function Sell.client_onEquippedUpdate(self, primaryState, secondaryState)
-	if not self.unlocked then return end
+	if not self.unlocked then return false, false end
 	-- Detect shape
 	local success, result = sm.localPlayer.getRaycast(7.5)
 	if result.type == "body" then
