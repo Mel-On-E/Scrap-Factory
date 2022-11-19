fa484426534fba08c9b4946ee5f83f02612289d3
Dr-Pixel-Plays <86497424+Dr-Pixel-Dev@users.noreply.github.com>
2022-11-19 19:40:20 +0100

    bouncium dropper

---
+++ Scripts/Drops/BouncyDrop.lua
@@ -0,0 +1,27 @@
+dofile("$CONTENT_DATA/Scripts/Drops/Drop.lua")
+
+---@class BouncyDrop : Drop
+---@field sv BouncyDrop
+---@diagnostic disable-next-line: param-type-mismatch, assign-type-mismatch
+BouncyDrop = class(Drop)
+
+local velocityLimit = 1000
+
+function BouncyDrop:server_onFixedUpdate()
+    Drop.server_onFixedUpdate(self)
+
+    if self.shape:getVelocity():length() > velocityLimit then
+        self.shape:destroyShape(0)
+    end
+end
+
+function BouncyDrop:server_onCollision(other, position, selfPointVelocity, otherPointVelocity, normal)
+
+
+    sm.physics.applyImpulse(self.shape, normal * 10, true)
+end
+
+--Types
+
+---@class GasDropSv : DropSv
+---@field startHeight number
