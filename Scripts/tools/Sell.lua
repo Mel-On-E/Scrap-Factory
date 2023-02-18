921650cb872bc1ccc8a1b16e7d50870373a6f222
Illumina <83984076+RedBoi-1@users.noreply.github.com>
2023-02-18 16:07:56 -0500

    Sell farticle for sell tool

--- Scripts/tools/Sell.lua
+++ Scripts/tools/Sell.lua
@@ -29,7 +29,7 @@ sm.tool.preloadRenderables(renderablesFp)
 
 function Sell.sv_n_sell(self, params, player)
 	if params.shape and sm.exists(params.shape) then
-		sm.effect.playEffect("Part - Upgrade", params.shape.worldPosition)
+		sm.effect.playEffect("Action - Sell", params.shape.worldPosition)
 		sm.event.sendToGame("sv_e_stonks",
 			{ pos = params.shape.worldPosition, value = tostring(params.value * params.quantity), format = "money" })
 
