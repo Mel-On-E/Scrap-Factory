abf34c9b45f50b69cc5db899fdef3fb6c27c9fc6
Dr-Pixel-Plays <86497424+Dr-Pixel-Dev@users.noreply.github.com>
2023-03-05 14:52:01 +0100

    Generator documentation

--- Scripts/Generators/Solar.lua
+++ Scripts/Generators/Solar.lua
@@ -1,16 +1,20 @@
 dofile("$CONTENT_DATA/Scripts/Generators/Generator.lua")
 dofile("$CONTENT_DATA/Scripts/util/day.lua")
 
-
+---A type of `Generator` that produces power only during daytime
 ---@class Solar : Generator
 Solar = class(Generator)
+
+--------------------
+-- #region Server
+--------------------
 
 function Solar:server_onCreate()
     Generator.server_onCreate(self)
     sm.event.sendToScriptableObject(g_tutorialManager.scriptableObject, "sv_e_tryStartTutorial", "SolarTutorial")
 end
 
-function Solar:getPower()
+function Solar:sv_getPower()
     local time = sm.game.getTimeOfDay()
     local timeMultiplier = 0
 
@@ -24,3 +28,5 @@ function Solar:getPower()
 
     return math.floor(timeMultiplier * self.data.power)
 end
+
+--#endregion
--- Scripts/Generators/Storage.lua
+++ Scripts/Generators/Storage.lua
@@ -1,13 +1,24 @@
 dofile("$CONTENT_DATA/Scripts/Generators/Generator.lua")
 dofile("$CONTENT_DATA/Scripts/Managers/LanguageManager.lua")
 
+---Storage is a type of `Generator` that produces no power but can store it.
 ---@class Storage : Generator
 Storage = class(Generator)
+
+--------------------
+-- #region Server
+--------------------
 
 function Storage:server_onCreate()
     Generator.server_onCreate(self)
     sm.event.sendToScriptableObject(g_tutorialManager.scriptableObject, "sv_e_tryStartTutorial", "PowerStorageTutorial")
 end
+
+--#endregion
+
+--------------------
+-- #region Server
+--------------------
 
 function Storage:client_canInteract()
     local o1 = "<p textShadow='false' bg='gui_keybinds_bg_orange' color='#4f4f4f' spacing='9'>"
@@ -21,3 +32,5 @@ function Storage:client_canInteract()
         o2)
     return true
 end
+
+--#endregion
