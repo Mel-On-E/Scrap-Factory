340a7438dc9cfd5bebad92bb8c54b5c7593c733e
Dr-Pixel-Plays <86497424+Dr-Pixel-Dev@users.noreply.github.com>
2023-03-05 18:33:39 +0100

    "Other" refactor & documentation

--- Scripts/Other/Belt.lua
+++ Scripts/Other/Belt.lua
@@ -1,6 +1,11 @@
+---A Belt is an item that has an areaTrigger. Everything inside that areaTrigger will be moved. It requires power to work.
 ---@class Belt : ShapeClass
 ---@field powerUtil PowerUtility
+---@field data BeltData
+---@field sv BeltSv
+---@field cl BeltCl
 Belt = class()
+
 Belt.maxParentCount = 1
 Belt.maxChildCount = 0
 Belt.connectionInput = sm.interactable.connectionType.logic
@@ -8,21 +13,28 @@ Belt.connectionOutput = sm.interactable.connectionType.none
 Belt.colorNormal = sm.color.new(0xdddd00ff)
 Belt.colorHighlight = sm.color.new(0xffff00ff)
 
+--------------------
+-- #region Server
+--------------------
+
+---@type table<number, boolean> table of all shape IDs that have been moved by a conveyor belt, so they won't get moved twice
 local IDsUpdated = {}
 
 function Belt:server_onCreate()
+    PowerUtility.sv_init(self)
+
+    --create areaTrigger
     local size = sm.vec3.new(self.data.belt.box.x, self.data.belt.box.y, self.data.belt.box.z)
     local offset = sm.vec3.new(self.data.belt.offset.x, self.data.belt.offset.y, self.data.belt.offset.z)
     self.sv = {}
     self.sv.trigger = sm.areaTrigger.createAttachedBox(self.interactable, size / 2, offset, sm.quat.identity(),
         sm.areaTrigger.filter.dynamicBody + sm.areaTrigger.filter.character)
     self.sv.trigger:bindOnStay("sv_onStay")
-
-    PowerUtility.sv_init(self)
 end
 
 function Belt:server_onFixedUpdate()
     PowerUtility.sv_fixedUpdate(self, "cl_toggleEffects")
+
     IDsUpdated = {}
 end
 
@@ -34,11 +46,12 @@ function Belt:sv_onStay(trigger, results)
             if result.id == self.shape.body.id then goto continue end
             if IDsUpdated[result.id] then goto continue end
 
+            --applyImpulse to each valid shape
             local direction = self.shape.at * self.data.belt.direction.at +
                 self.shape.right * self.data.belt.direction.right +
                 self.shape.up * self.data.belt.direction.up
             local force = direction * (result.mass / 4) * self.data.belt.speed
-            local dirVelocity = getDirectionalVelocity(result:getVelocity(), direction)
+            local dirVelocity = self.getDirectionalVelocity(result:getVelocity(), direction)
 
             if dirVelocity:length() < self.data.belt.speed then
                 sm.physics.applyImpulse(result, force, true)
@@ -49,10 +62,36 @@ function Belt:sv_onStay(trigger, results)
     end
 end
 
+---Calculate the directional velocity that should be applied to shapes affected by the belt.
+---@param vel Vec3 the velocity to be applied to a shape
+---@param dir Vec3 the direction the velocity should push shapes
+---@return Vec3 dirVel the directional velocity to apply to shapes
+function Belt.getDirectionalVelocity(vel, dir)
+    local dimensions = { "x", "y", "z" }
+    local dirVelocity = sm.vec3.zero()
+
+    for _, dim in ipairs(dimensions) do
+        if dir[dim] > 0 and vel[dim] > 0 then
+            dirVelocity[dim] = dir[dim] * vel[dim]
+        elseif dir[dim] < 0 and vel[dim] < 0 then
+            dirVelocity[dim] = dir[dim] * vel[dim]
+        end
+    end
+
+    return dirVelocity
+end
+
+-- #endregion
+
+--------------------
+-- #region Client
+--------------------
+
 function Belt:client_onCreate()
-    self.cl = {}
-    self.cl.uvIndex = 0
-    self.cl.active = true
+    self.cl = {
+        uvIndex = 0,
+        active = true
+    }
 end
 
 function Belt:cl_toggleEffects(active)
@@ -69,17 +108,36 @@ function Belt:client_onUpdate(dt)
     end
 end
 
-function getDirectionalVelocity(vel, dir)
-    local dimensions = { "x", "y", "z" }
-    local dirVelocity = sm.vec3.zero()
+-- #endregion
 
-    for _, dim in ipairs(dimensions) do
-        if dir[dim] > 0 and vel[dim] > 0 then
-            dirVelocity[dim] = dir[dim] * vel[dim]
-        elseif dir[dim] < 0 and vel[dim] < 0 then
-            dirVelocity[dim] = dir[dim] * vel[dim]
-        end
-    end
+--------------------
+-- #region Types
+--------------------
 
-    return dirVelocity
-end
+---@class BeltSv
+---@field trigger AreaTrigger area in which shapes get moved
+
+---@class BeltData
+---@field belt BeltDataBelt
+
+---@class BeltDataBelt
+---@field speed number the maximum speed of the belt. Only shapes slower than this will be accelerated.
+---@field box BeltVec size of the areaTrigger
+---@field offset BeltVec offset of the areaTrigger
+---@field direction BeltRotStuffPlsHelpIHateNamingThis direction in which shapes are pushed
+
+---@class BeltVec
+---@field x number
+---@field y number
+---@field z number
+
+---@class BeltRotStuffPlsHelpIHateNamingThis
+---@field up number
+---@field right number
+---@field at number
+
+---@class BeltCl
+---@field active boolean if the belt is currently turned on or off
+---@field uvIndex number the current UV index of the belt
+
+-- #endregion
--- Scripts/Other/DaySensor.lua
+++ Scripts/Other/DaySensor.lua
@@ -1,34 +1,37 @@
-dofile("$CONTENT_DATA/Scripts/util/day.lua")
-
----@class DayDetector : ShapeClass
----@field day boolean
+---A DaySensor is a sensor that will emit a logic signal during daytime
+---@class DaySensor : ShapeClass
 ---@diagnostic disable-next-line: assign-type-mismatch
 DaySensor = class()
+
 DaySensor.connectionOutput = sm.interactable.connectionType.logic
 DaySensor.maxChildCount = 255
 DaySensor.poseWeightCount = 1
 DaySensor.colorNormal = sm.color.new(0x9a0d44ff)
 DaySensor.colorHighlight = sm.color.new(0xc01559ff)
 
-local enabledPose = 10
+--------------------
+-- #region Server
+--------------------
 
 function DaySensor:server_onCreate()
-    self.sv = {}
-    self.sv.day = false
-
     self.network:sendToClients("cl_changeModel")
 end
 
 function DaySensor:server_onFixedUpdate()
-    local day = isDay()
-
-    if day == self.sv.day then return end
-
-    self.interactable:setActive(day)
-    self.network:sendToClients("cl_changeModel")
-    self.sv.day = day
+    if self.interactable.active ~= isDay() then
+        self.interactable:setActive(isDay())
+        self.network:sendToClients("cl_changeModel")
+    end
 end
+
+-- #endregion
+
+--------------------
+-- #region Client
+--------------------
 
 function DaySensor:cl_changeModel()
     self.interactable:setPoseWeight(0, self.interactable.active and 0 or 1)
 end
+
+-- #endregion
