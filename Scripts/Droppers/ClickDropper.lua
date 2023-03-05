3fe48ee9868ca1b66c45a19e58737f791e813a3f
Dr-Pixel-Plays <86497424+Dr-Pixel-Dev@users.noreply.github.com>
2023-03-05 18:58:31 +0100

    thx Vajdani UwU

--- Scripts/Droppers/ClickDropper.lua
+++ Scripts/Droppers/ClickDropper.lua
@@ -19,8 +19,8 @@ function ClickDropper:server_onFixedUpdate()
     self.sv.prevState = state
 end
 
-function ClickDropper:sv_toggleActive()
-    self.interactable:setActive(not self.interactable.active)
+function ClickDropper:sv_setActive(state)
+    self.interactable.active = state
     self.network:sendToClients("client_playSound", "Button " .. (self.interactable.active and "off" or "on"))
 end
 
@@ -41,40 +41,10 @@ function ClickDropper:client_onFixedUpdate()
         self.interactable:setPoseWeight(0, state and 1 or 0)
     end
     self.cl.prevState = state
-
-    --check if the player doesn't look at the dropper anymore
-    local char = sm.localPlayer.getPlayer().character
-    if char and not self.cl.look and char:getLockingInteractable() == self.interactable then
-        ---@diagnostic disable-next-line: param-type-mismatch
-        char:setLockingInteractable(nil)
-        self.network:sendToServer("sv_toggleActive")
-    end
-    self.cl.look = false
 end
 
 function ClickDropper:client_onInteract(character, state)
-    if state == true then
-        --when the player presses the button
-        self.network:sendToServer("sv_toggleActive")
-        character:setLockingInteractable(self.interactable)
-    end
-end
-
-function ClickDropper:client_onAction(controllerAction, state)
-    if not state and controllerAction == 15 then
-        --when the player releases the button
-        ---@diagnostic disable-next-line: param-type-mismatch
-        sm.localPlayer.getPlayer().character:setLockingInteractable(nil)
-        self.network:sendToServer("sv_toggleActive")
-    end
-
-    return false
-end
-
-function ClickDropper:client_canInteract(character, state)
-    --check if the player is looking at the dropper
-    self.cl.look = true
-    return true
+    self.network:sendToServer("sv_setActive", state)
 end
 
 function ClickDropper:client_playSound(name)
@@ -91,8 +61,6 @@ end
 ---@field prevState boolean the state of the dropper 1 tick earlier
 
 ---@class ClickDropperCl
----@field cl table
----@field look boolean whehter the player is looking at the dropper
 ---@field prevState boolean the state of the dropper 1 tick earlier
 
 -- #endregion
