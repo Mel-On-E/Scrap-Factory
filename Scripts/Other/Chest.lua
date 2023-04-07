4f37dd2f0e34af7c226e60e7b495af36b10bc7cd
Donut05 <87012174+Donut05@users.noreply.github.com>
2023-04-07 22:28:22 +0300

    Sorting effects

--- Scripts/Other/Chest.lua
+++ Scripts/Other/Chest.lua
@@ -5,6 +5,7 @@ dofile("$SURVIVAL_DATA/Scripts/game/survival_loot.lua")
 ---@field sv ChestSv
 ---@field cl ChestCl
 Chest = class(nil)
+Chest.poseWeightCount = 1
 
 --------------------
 -- #region Server
@@ -26,7 +27,8 @@ function Chest.server_onCreate(self)
 	self.sv = {
 		container = container,
 		lootList = {},
-		cachedPos = self.shape.worldPosition
+		cachedPos = self.shape.worldPosition,
+		playersHavingChestGuiOpen = 0
 	}
 end
 
@@ -55,28 +57,55 @@ function Chest.server_canErase(self)
 	return self.sv.container:isEmpty()
 end
 
+function Chest.sv_openChestAnim(self)
+	self.sv.playersHavingChestGuiOpen = self.sv.playersHavingChestGuiOpen + 1
+	if self.sv.playersHavingChestGuiOpen == 1 then
+		self.network:sendToClients("cl_openChestAnim")
+	end
+end
+
+function Chest.sv_closeChestAnim(self)
+	self.sv.playersHavingChestGuiOpen = self.sv.playersHavingChestGuiOpen - 1
+	if self.sv.playersHavingChestGuiOpen == 0 then
+		self.network:sendToClients("cl_closeChestAnim")
+	end
+end
+
 -- #endregion
 
 --------------------
 -- #region Client
 --------------------
 
+local chestOpeningSpeed = 8.0
+
 function Chest:client_onCreate()
-	self.cl = { container = self.shape.interactable:getContainer(0) }
+	self.cl = {}
+	self.cl.chestAnimDirection = -1
 end
 
 function Chest.client_onInteract(self, character, state)
 	if state == true then
-		if self.cl.container then
+		local container = self.shape.interactable:getContainer(0)
+		if container then
 			self.cl.containerGui = sm.gui.createContainerGui(true)
 			self.cl.containerGui:setText("UpperName", "#{CONTAINER_TITLE_GENERIC}")
 			self.cl.containerGui:setVisible("TakeAll", true)
-			self.cl.containerGui:setContainer("UpperGrid", self.shape.interactable:getContainer(0));
+			self.cl.containerGui:setContainer("UpperGrid", container);
 			self.cl.containerGui:setText("LowerName", "#{INVENTORY_TITLE}")
 			self.cl.containerGui:setContainer("LowerGrid", sm.localPlayer.getInventory())
+			self.cl.containerGui:setOnCloseCallback("cl_guiClosed")
 			self.cl.containerGui:open()
+
+			sm.effect.playEffect("Action - Chest Open", self.shape.worldPosition)
+			self.network:sendToServer("sv_openChestAnim")
 		end
 	end
+end
+
+function Chest.cl_guiClosed(self)
+	self.network:sendToServer("sv_closeChestAnim")
+	sm.effect.playEffect("Action - Chest Close", self.shape.worldPosition)
 end
 
 function Chest.client_onDestroy(self)
@@ -86,6 +115,21 @@ function Chest.client_onDestroy(self)
 			self.cl.containerGui:destroy()
 		end
 	end
+end
+
+function Chest.cl_openChestAnim(self)
+	self.cl.chestAnimDirection = 1
+end
+
+function Chest.cl_closeChestAnim(self)
+	self.cl.chestAnimDirection = -1
+end
+
+function Chest.client_onUpdate(self, dt)
+	local poseWeight = self.interactable:getPoseWeight(0)
+	poseWeight = poseWeight + (chestOpeningSpeed * self.cl.chestAnimDirection) * dt
+	poseWeight = sm.util.clamp(poseWeight, 0, 1)
+	self.interactable:setPoseWeight(0, poseWeight)
 end
 
 -- #endregion
@@ -98,10 +142,11 @@ end
 ---@field container Container
 ---@field cachedPos Vec3 cached position of the Chest
 ---@field lootList table <number, Item> list of all items in a Chest
+---@field playersHavingChestGuiOpen integer how many players have the chest opened rn
 
 ---@class ChestCl
----@field container Container
 ---@field containerGui GuiInterface gui that is visible when opening the chest
+---@field chestAnimDirection -1|1 whehter the chest keeps opening or closing
 
 
 -- #endregion
