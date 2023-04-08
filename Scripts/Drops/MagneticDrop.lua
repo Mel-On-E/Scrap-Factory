d70035e39b7e6e3a6ee2856ee91370116e337260
Dr-Pixel-Plays <86497424+Dr-Pixel-Dev@users.noreply.github.com>
2023-04-08 20:47:13 +0200

    refactor: using areaTriggers for magnetic force

--- Scripts/Drops/MagneticDrop.lua
+++ Scripts/Drops/MagneticDrop.lua
@@ -10,50 +10,61 @@ MagneticDrop = class(Drop)
 -- #region Server
 --------------------
 
-local magneticDrops = {
-    tick = sm.game.getCurrentTick(),
-    drops = {}
-}
+local triggerSize = sm.vec3.one() * 8
 
 function MagneticDrop:server_onCreate()
     Drop.server_onCreate(self)
 
-    magneticDrops.drops[#magneticDrops.drops + 1] = {
-        south = self.data.south,
-        shape = self.shape
-    }
+    self.interactable.publicData.magnetic = self.data.south and "south" or "north"
+    self.sv.trigger = sm.areaTrigger.createAttachedBox(self.interactable, triggerSize / 2, sm.vec3.zero(),
+        sm.quat.identity(),
+        sm.areaTrigger.filter.dynamicBody)
+    self.sv.trigger:bindOnStay("sv_onStay")
 end
 
-function MagneticDrop:server_onFixedUpdate()
-    Drop.server_onFixedUpdate(self)
+function MagneticDrop:sv_onStay(trigger, results)
+    local shapes = self:sv_getValidShapes(results)
 
-    --update magnetic drops
-    if magneticDrops.tick < sm.game.getCurrentTick() then
-        magneticDrops.tick = sm.game.getCurrentTick()
+    for _, shape in ipairs(shapes) do
+        local publicData = shape.interactable.publicData
 
-        --clear old drops
-        for k, v in pairs(magneticDrops.drops) do
-            if not v.shape or not sm.exists(v.shape) then
-                magneticDrops.drops[k] = nil
-            end
-        end
+        if publicData.magnetic then
+            local direction = (self.interactable.publicData.magnetic == publicData.magnetic and -1) or 1
+            local distance = shape.worldPosition - self.shape.worldPosition
+            local factor = 1 / distance:length()
+            local force = distance:normalize() * direction * factor
 
-        --apply magnetic force to each drop
-        for k1, v1 in pairs(magneticDrops.drops) do
-            local force = sm.vec3.zero()
-            for k2, v2 in pairs(magneticDrops.drops) do
-                if v1 == v2 then goto continue end
-
-                local direction = (v1.south == v2.south and -1) or 1
-                local distance = v2.shape.worldPosition - v1.shape.worldPosition
-                local factor = 1 / distance:length()
-                force = force + distance:normalize() * direction * factor
-
-                ::continue::
-            end
-            sm.physics.applyImpulse(v1.shape:getBody(), force, true)
+            sm.physics.applyImpulse(self.shape:getBody(), force, true)
         end
     end
+end
+
+---@param results table table of shapes to be validated
+---@return table<integer, Shape> shapes a list of shapes that are vaild
+function MagneticDrop:sv_getValidShapes(results)
+    local shapes = {}
+    for _, result in ipairs(results) do
+        if sm.exists(result) then
+            if type(result) ~= "Body" then goto continue end
+            if result.id == self.shape.body.id then goto continue end
+
+            if #result:getShapes() > 1 then goto continue end
+
+            local shape = result:getShapes()[1]
+            local interactable = shape:getInteractable()
+
+            if not interactable then goto continue end
+            if interactable.type ~= "scripted" then goto continue end
+
+            local data = interactable:getPublicData()
+            if not data or not data.value then goto continue end
+
+            shapes[#shapes + 1] = shape
+        end
+        ::continue::
+    end
+
+    return shapes
 end
 
 -- #endregion
