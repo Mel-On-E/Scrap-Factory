b0159aa6f65bfd426da1bf3382cc77623071ab0d
Tofixrs <tofix.rs@proton.me>
2023-04-16 14:19:09 +0200

    feat: color mogus

--- Scripts/Drops/SusDrop.lua
+++ Scripts/Drops/SusDrop.lua
@@ -4,10 +4,30 @@ dofile("$CONTENT_DATA/Scripts/Drops/Drop.lua")
 ---@class SusDrop : Drop
 ---@diagnostic disable-next-line: param-type-mismatch, assign-type-mismatch
 SusDrop = class(Drop)
-
--- #region Server 
+local colors = {
+	"#132ed2",
+	"#11802d",
+	"#ee54bb",
+	"#f07d0d",
+	"#f6f657",
+	"#3f474e",
+	"#d7e1f1",
+	"#6b2fbc",
+	"#71491e",
+	"#38e2dd",
+	"#50f039",
+	"#6b2b3c",
+	"#ecc0d3",
+	"#fffebe",
+	"#708496",
+	"#928776",
+	"#ec7578",
+}
+-- #region Server
 function SusDrop:server_onCreate()
 	Drop.server_onCreate(self)
+
+	self.shape:setColor(sm.color.new(colors[math.random(1, #colors)]))
 
 	if math.random(1, 10) ~= 10 then
 		return
@@ -17,6 +37,7 @@ function SusDrop:server_onCreate()
 	data.impostor = true
 
 	self.interactable:setPublicData(data)
+	self.shape:setColor(sm.color.new("#c61111"))
 end
 
 -- #endregion
