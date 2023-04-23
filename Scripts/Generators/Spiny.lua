0b6fb9b5a14561321ac27115d1b238e2ed979c81
Natejoestev <85044853+Natejoestev@users.noreply.github.com>
2023-04-23 12:02:23 -0700

    create spiny power thing show errors on hover gen power when spinning

---
+++ Scripts/Generators/Spiny.lua
@@ -0,0 +1,71 @@
+dofile("$CONTENT_DATA/Scripts/Generators/Generator.lua")
+
+---A type of generator that will produce power when spun.
+---@class Spiny : Generator
+---@field active boolean weather it is producing power
+---@field pvActive boolean previus frame active
+Spiny = class(Generator)
+
+local tagStatic = language_tag('SpinyGeneratorStatic')
+local tagBearing = language_tag('SpinyGeneratorBearing')
+
+--------------------
+-- #region Server
+--------------------
+
+function Spiny:server_onCreate()
+    Generator.server_onCreate(self)
+end
+
+function Spiny:sv_getPower()
+    if not self.active then return 0 end
+    return math.floor(self.shape.body.angularVelocity:length()/120 *4)
+end
+
+function Spiny:server_onFixedUpdate(dt)
+    Generator.server_onFixedUpdate(self)
+    self.active = false
+    local reason = 0
+    for _,joint in ipairs(self.shape.body:getCreationJoints()) do
+        joint=joint ---@type Joint
+        if joint.shapeB == self.shape then
+            self.active = true
+            if not joint.shapeA.body:isStatic() then
+                self.active = false
+                reason = 1
+            end
+            break
+        end
+    end
+    if self.active ~= self.pvActive then
+        self.network:setClientData({active=self.active, reason=reason})
+    end
+    self.pvActive = self.active
+end
+
+-- #endregion
+
+
+--------------------
+-- #region Client
+--------------------
+
+function Spiny:client_onClientDataUpdate(data)
+    if data.active ~= nil then
+        self.active = data.active
+        self.reason = data.reason
+    else
+        Generator.client_onClientDataUpdate(self, data)
+    end
+end
+
+function Spiny:client_canInteract()
+    if self.active then
+        return Generator.client_canInteract(self)
+    end
+    local s = self.reason==1 and tagStatic or tagBearing
+    sm.gui.setInteractionText("<p textShadow='false' bg='gui_keybinds_bg_orange' color='#db2a16' spacing='9'>"..s.."</p>")
+    return true
+end
+
+-- #endregion
