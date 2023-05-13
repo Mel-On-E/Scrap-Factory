00e7cde833ac5223ef7f146c5b79230d8b56cf1f
Natejoestev <85044853+Natejoestev@users.noreply.github.com>
2023-05-12 20:52:08 -0700

    refactor popped

--- Scripts/Drops/PopcornDrop.lua
+++ Scripts/Drops/PopcornDrop.lua
@@ -2,13 +2,13 @@ dofile("$CONTENT_DATA/Scripts/Drops/Drop.lua")
 
 ---A PopcornDrop is a drop ...
 ---@class PopcornDrop : Drop
----@field sv PopcornSv
+---@field popped boolean is this drop popped
 PopcornDrop = class(Drop)
 
 local popTimeMin = 40 * 1.5
 local popTimeMax = 40 * 3
 
-local popedUuid = sm.uuid.new("b1b00f76-b8eb-4d64-93cc-a6e5bfd6fe40")
+local poppedUuid = sm.uuid.new("b1b00f76-b8eb-4d64-93cc-a6e5bfd6fe40")
 
 --------------------
 -- @region Server
@@ -16,18 +16,19 @@ local popedUuid = sm.uuid.new("b1b00f76-b8eb-4d64-93cc-a6e5bfd6fe40")
 
 function PopcornDrop:server_onCreate()
     Drop.server_onCreate(self)
-    self.sv.poped = self.shape:getShapeUuid() == popedUuid
+    self.popped = self.shape:getShapeUuid() == poppedUuid
     self.sv.time = sm.noise.randomRange(popTimeMin, popTimeMax)
 end
 
 function PopcornDrop:server_onFixedUpdate()
     Drop.server_onFixedUpdate(self)
-    if not self.sv.poped then
+    if not self.popped then
         self.sv.time = self.sv.time - 1
         if self.sv.time <= 0 then
+            --i honestly don't know why this offset is needed. but without it, it hovers in the air :/
             sm.effect.playEffect( "Cotton - Picked", self.shape.worldPosition-sm.vec3.new(0,0,1) )
             self.shape:destroyShape(0)
-            local shape = sm.shape.createPart(popedUuid, self.shape.worldPosition, self.shape.worldRotation)
+            local shape = sm.shape.createPart(poppedUuid, self.shape.worldPosition, self.shape.worldRotation)
             local publicData = {
                 value = self:getValue() * 2, --TODO balance multiplier
                 pollution = nil,
@@ -38,14 +39,5 @@ function PopcornDrop:server_onFixedUpdate()
         end
     end
 end
--- #endregion
-
---------------------
--- @region Types
---------------------
-
----@class PopcornSv : DropSv
----@field time number time remaining until it pops
----@field poped boolean is it poped
 
 -- #endregion
