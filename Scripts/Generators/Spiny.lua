19cc99a6995684b5f29813af4bd5cae80d9a3fc7
Natejoestev <85044853+Natejoestev@users.noreply.github.com>
2023-05-21 08:27:36 -0700

    refactor move check to onCreate

--- Scripts/Generators/Spiny.lua
+++ Scripts/Generators/Spiny.lua
@@ -3,24 +3,19 @@ dofile("$CONTENT_DATA/Scripts/Generators/Generator.lua")
 ---A type of generator that will produce power when spun.
 ---@class Spiny : Generator
 ---@field active boolean weather it is producing power
----@field pvActive boolean previus frame active
 Spiny = class(Generator)
 
 --------------------
 -- #region Server
 --------------------
 
-function Spiny:sv_getPower()
-    if not self.active then return 0 end
-    return math.floor(self.shape.body.angularVelocity:length()/120 *4)
-end
-
-function Spiny:server_onFixedUpdate(dt)
-    Generator.server_onFixedUpdate(self)
+function Spiny:server_onCreate()
+    Generator.server_onCreate(self)
     self.active = false
+    -- 0 means placed on block   
+    -- 1 means non static
     local reason = 0
     for _,joint in ipairs(self.shape.body:getCreationJoints()) do
-        joint=joint ---@type Joint
         if joint.shapeB == self.shape then
             self.active = true
             if not joint.shapeA.body:isStatic() then
@@ -30,10 +25,12 @@ function Spiny:server_onFixedUpdate(dt)
             break
         end
     end
-    if self.active ~= self.pvActive then
-        self.network:setClientData({active=self.active, reason=reason})
-    end
-    self.pvActive = self.active
+    self.network:setClientData({active=self.active, reason=reason})
+end
+
+function Spiny:sv_getPower()
+    if not self.active then return 0 end
+    return math.floor(self.shape.body.angularVelocity:length()/120 *4)
 end
 
 -- #endregion
@@ -53,14 +50,14 @@ function Spiny:client_onDestroy()
 end
 
 function Spiny:client_onClientDataUpdate(data)
-    if data.active ~= nil then
+    if data.power then
+        Generator.client_onClientDataUpdate(self, data)
+    else
         self.active = data.active
         self.reason = data.reason
         if data.active then
             Effects.cl_createEffect(self, { key = "afct", effect = "Spiny Power", host = self.interactable })
         end
-    else
-        Generator.client_onClientDataUpdate(self, data)
     end
 end
 
