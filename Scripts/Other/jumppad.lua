5cf93e149c2346d9e88ca63344b3d6787f9a2fdd
Natejoestev <85044853+Natejoestev@users.noreply.github.com>
2023-05-22 22:27:02 -0700

    add effect and fix regions

--- Scripts/Other/jumppad.lua
+++ Scripts/Other/jumppad.lua
@@ -8,7 +8,9 @@ Jumppad.connectionInput = sm.interactable.connectionType.logic
 Jumppad.colorNormal = sm.color.new(0x2222ddff)
 Jumppad.colorHighlight = sm.color.new(0x4444ffff)
 
--- Server
+--------------------
+-- #region Server
+--------------------
 
 function Jumppad:server_onCreate()
     PowerUtility.sv_init(self)
@@ -63,12 +65,40 @@ function Jumppad:server_onFixedUpdate(dt)
     PowerUtility.sv_fixedUpdate(self, "cl_toggleEffect")
 end
 
--- Client
+-- #endregion
+
+--------------------
+-- #region Client
+--------------------
 
 function Jumppad:client_onCreate()
-    
+    self.cl = {}
+
+    --create portal area effect
+    local size = sm.vec3.new(self.data.box.x, self.data.box.y * 7.5, self.data.box.z)
+    local offset = sm.vec3.new(self.data.offset.x, self.data.offset.y, self.data.offset.z)
+
+    self.cl.effect = sm.effect.createEffect("ShapeRenderable", self.interactable)
+    self.cl.effect:setParameter("uuid", sm.uuid.new("f74a0354-05e9-411c-a8ba-75359449f770"))
+    self.cl.effect:setParameter("color", sm.color.new('#f0580c'))
+    self.cl.effect:setScale(size / 4.5)
+    self.cl.effect:setOffsetPosition(offset)
+    local rot1 = sm.vec3.getRotation(sm.vec3.new(0, 0, 1), sm.vec3.new(0, 1, 0))
+
+    --really fucking weird rotation offset thingy bc epic shader doesn't work on all rotations. WTF axolot why?
+    local rot2 = self.shape.xAxis.y ~= 0 and sm.vec3.getRotation(sm.vec3.new(1, 0, 0), sm.vec3.new(0, 1, 0)) or
+        sm.quat.identity()
+    self.cl.effect:setOffsetRotation(rot1 * rot2)
+
+    self.cl.effect:start()
 end
 
 function Jumppad:cl_toggleEffect(active)
-    
+    if active and not self.cl.effect:isPlaying() then
+        self.cl.effect:start()
+    else
+        self.cl.effect:stop()
+    end
 end
+
+-- #endregion
