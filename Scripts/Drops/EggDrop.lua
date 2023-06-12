b112801f83a38b7e2eb03fb1bd6f3ad65380c764
Natejoestev <85044853+Natejoestev@users.noreply.github.com>
2023-06-12 14:21:39 -0700

    create egg drop

---
+++ Scripts/Drops/EggDrop.lua
@@ -0,0 +1,25 @@
+dofile("$CONTENT_DATA/Scripts/Drops/Drop.lua")
+
+---An EggDrop is a drop that is egg.
+---@class EggDrop: Drop
+EggDrop = class(Drop)
+
+--------------------
+-- @region Server
+--------------------
+
+function EggDrop:server_onCreate()
+    Drop.server_onCreate(self)
+end
+
+-- #endregion
+
+--------------------
+-- @region Client
+--------------------
+
+function EggDrop:client_onCreate()
+    Drop.client_onCreate(self)
+end
+
+-- #endregion
