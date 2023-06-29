953d1ce16305bd0e32f30c7d3362d343cb8e533a
Natejoestev <85044853+Natejoestev@users.noreply.github.com>
2023-06-28 18:34:07 -0700

    add code to "suck" value out of drop.

--- Scripts/Drops/VampireDrop.lua
+++ Scripts/Drops/VampireDrop.lua
@@ -2,9 +2,12 @@ dofile("$CONTENT_DATA/Scripts/Drops/Drop.lua")
 
 ---A VampireDrop is a drop that 
 ---@class VampireDrop : Drop
+---@field suckDelay number ticks until it can suck again (yes)
 VampireDrop = class(Drop)
 
-local durability = 4
+local coefficient = 0.8
+--time until a vampire drop can suck again
+local suckDelayTime = 3 * 40
 
 --------------------
 -- #region Server
@@ -12,15 +15,37 @@ local durability = 4
 
 function VampireDrop:server_onCreate()
     Drop.server_onCreate(self)
+    self.suckDelay = 0
 end
 
-function VampireDrop:server_onMeleeDrop(position, attacker, damage, power, direction, normal)
-    if (damage>durability) then
-        self.shape:destroyPart()
+function VampireDrop:server_onCollision(other, position, selfPointVelocity, otherPointVelocity, normal)
+    if type(other) == "Shape" and self.suckDelay < 1 then
+        self.suckDelay = suckDelayTime
+        ---@cast other Shape
+        local interactable = other:getInteractable()
+        if not interactable or interactable.type ~= "scripted" then goto continue end
+        local data = interactable:getPublicData()
+        if not data or not data.value then goto continue end
+        -- varify that the collided shape is a drop.
+
+        --TODO maybe have some money lost
+        local stole = coefficient * data.value
+        data.value = data.value - stole --take from drop
+        self.interactable.publicData.value = self.interactable.publicData.value + stole --add to
+        sm.event.sendToPlayer(sm.player.getAllPlayers()[1], "sv_e_numberEffect", {
+            pos = other.worldPosition,
+            value = tostring(stole),
+            color = '#de431d'
+        })
+        --TODO maybe add boost or smth (like side effect of getting bit by a vampire)
+        :: continue ::
     end
+    Drop.server_onCollision(self, other) -- destroy shape if touching ground
 end
+
+--TODO hide in light
+
+-- #endregion
 
 -- ERROR the shape collides with the spawner (line 1996 in droppers.shapeset)
 -- TODO destroy explosion guts effect
-
--- #endregion
