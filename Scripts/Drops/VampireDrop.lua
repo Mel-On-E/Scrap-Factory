b15ce6f3d8faf710c7641d07cbe5e76368d85413
MelOnE <86497424+Mel-On-E@users.noreply.github.com>
2023-07-05 20:55:18 +0200

    refactor

--- Scripts/Drops/VampireDrop.lua
+++ Scripts/Drops/VampireDrop.lua
@@ -1,12 +1,11 @@
 dofile("$CONTENT_DATA/Scripts/Drops/Drop.lua")
 
----A VampireDrop is a drop that 
+---A VampireDrop is a drop that
 ---@class VampireDrop : Drop
----@field suckDelay number ticks until it can suck again (yes)
+---@field sv VampireDropSv
 VampireDrop = class(Drop)
 
-local coefficient = 0.8
---time until a vampire drop can suck again
+local suckFraction = 0.8
 local suckDelayTime = 3 * 40
 
 --------------------
@@ -15,12 +14,12 @@ local suckDelayTime = 3 * 40
 
 function VampireDrop:server_onCreate()
     Drop.server_onCreate(self)
-    self.suckDelay = 0
+    self.sv.suckDelay = 0
 end
 
 function VampireDrop:server_onCollision(other, position, selfPointVelocity, otherPointVelocity, normal)
-    if type(other) == "Shape" and self.suckDelay < 1 then
-        self.suckDelay = suckDelayTime
+    if type(other) == "Shape" and self.sv.suckDelay <= 0 then
+        self.sv.suckDelay = suckDelayTime
         ---@cast other Shape
         local interactable = other:getInteractable()
         if not interactable or interactable.type ~= "scripted" then goto continue end
@@ -28,16 +27,15 @@ function VampireDrop:server_onCollision(other, position, selfPointVelocity, othe
         if not data or not data.value then goto continue end
         -- varify that the collided shape is a drop.
 
-        --TODO maybe have some money lost
-        local stole = coefficient * data.value
-        data.value = data.value - stole --take from drop
-        self.interactable.publicData.value = self.interactable.publicData.value + stole --add to
+        local stolenValue = suckFraction * data.value
+        data.value = math.sqrt(data.value)
+        self.interactable.publicData.value = self.interactable.publicData.value + stolenValue
+
         sm.event.sendToPlayer(sm.player.getAllPlayers()[1], "sv_e_numberEffect", {
             pos = other.worldPosition,
-            value = tostring(stole),
+            value = tostring(stolenValue),
             color = '#de431d'
         })
-        --TODO maybe add boost or smth (like side effect of getting bit by a vampire)
         :: continue ::
     end
     Drop.server_onCollision(self, other) -- destroy shape if touching ground
@@ -49,3 +47,12 @@ end
 
 -- ERROR the shape collides with the spawner (line 1996 in droppers.shapeset)
 -- TODO destroy explosion guts effect
+
+--------------------
+-- #region Types
+--------------------
+
+---@class VampireDropSv : DropSv
+---@field suckDelay number time until a vampire drop can suck again
+
+-- #endregion
